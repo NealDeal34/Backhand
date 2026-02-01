4eedf4526ebf3321a29ac4b03c9a3b16dd7e68b4
NealDeal34 <130116696+NealDeal34@users.noreply.github.com>
2026-02-01 14:04:48 +0800

    Enhance null safety in MixinGuiToolConfig
    Added null checks for player and inventory in the backhand$addDependentComponents method.

--- src/main/java/xonin/backhand/mixins/late/draconicevolution/MixinGuiToolConfig.java
+++ src/main/java/xonin/backhand/mixins/late/draconicevolution/MixinGuiToolConfig.java
@@ -2,27 +2,28 @@ package xonin.backhand.mixins.late.draconicevolution;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.Container;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
-
-import com.brandon3055.brandonscore.client.gui.guicomponents.GUIBase;
 import com.brandon3055.draconicevolution.client.gui.componentguis.GUIToolConfig;
+import com.brandon3055.draconicevolution.client.utils.guicomponents.GUIBase;
 
 @Mixin(value = GUIToolConfig.class, remap = false)
 public abstract class MixinGuiToolConfig extends GUIBase {
 
-    @Shadow
+    @Shadow(remap = false)
     public EntityPlayer player;
 
     public MixinGuiToolConfig(Container container, int xSize, int ySize) {
         super(container, xSize, ySize);
     }
 
-    @ModifyConstant(method = "addDependentComponents", constant = @Constant(intValue = 39))
+    @ModifyConstant(method = "addDependentComponents", constant = @Constant(intValue = 39), remap = false)
     private int backhand$addDependentComponents(int constant) {
-        return player.inventory.getSizeInventory() - 1;
+        if (player != null && player.inventory != null) {
+            return player.inventory.getSizeInventory() - 1;
+        }
+        return constant;
     }
 }
